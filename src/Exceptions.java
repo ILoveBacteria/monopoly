c15d5a72c213d9df0e3de88023f4805b234fac9e
Ali Tabatabei <alitaabaa@gmail.com>
2022-04-04 13:53:53 +0430

    Exceptions class has been added.

---
+++ src/Exceptions.java
@@ -0,0 +1,61 @@
+public class Exceptions extends Exception{
+}
+
+class AreaWithoutRentException extends Exception{
+    public AreaWithoutRentException() {
+        System.out.println("AreaWithoutRentException");
+    }
+}
+
+class InsufficientAssetException extends Exception{
+    public InsufficientAssetException() {
+        System.out.println("InsufficientAssetException");
+    }
+}
+
+class UnPurchasableAreaException extends Exception{
+    public UnPurchasableAreaException() {
+        System.out.println("UnPurchasableAreaException");
+    }
+}
+
+class UnrelatedBuyerException extends Exception{
+    public UnrelatedBuyerException() {
+        System.out.println("UnrelatedBuyerException");
+    }
+}
+
+class UnrelatedUserException extends Exception{
+    public UnrelatedUserException() {
+        System.out.println("UnrelatedUserException");
+    }
+}
+
+class UnrelatedAreaException extends Exception {
+    UnrelatedAreaException() {
+        System.out.println("UnrelatedAreaException");
+    }
+}
+
+class NotInJailException extends Exception{
+    public NotInJailException() {
+        System.out.println("NotInJailException");
+    }
+}
+
+class MaxHotelBuiltException extends Exception{
+    public MaxHotelBuiltException() {
+        System.out.println("MaxHotelBuiltException");
+    }
+}
+class MaxHousesBuiltException extends Exception{
+    public MaxHousesBuiltException() {
+        System.out.println("MaxHousesBuiltException");
+    }
+}
+
+class UnableToBuildException extends Exception{
+    public UnableToBuildException() {
+        System.out.println("UnableToBuildException");
+    }
+}
