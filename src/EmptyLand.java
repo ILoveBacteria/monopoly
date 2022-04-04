fdada098f9795077b402508a65082cdbbfe854b0
Ali Tabatabei <alitaabaa@gmail.com>
2022-04-04 13:53:32 +0430

    2 new variables have been added.

--- src/EmptyLand.java
+++ src/EmptyLand.java
@@ -1,6 +1,26 @@
 public class EmptyLand extends Area {
+
+    private int housesCount = 0;
+    private int hotelsCount = 0;
+
     public EmptyLand(int areaNumber) {
         super(areaNumber, true);
         super.setBuyPrice(100);
+    }
+
+    public int getHousesCount() {
+        return housesCount;
+    }
+
+    public void setHousesCount(int housesCount) {
+        this.housesCount = housesCount;
+    }
+
+    public int getHotelsCount() {
+        return hotelsCount;
+    }
+
+    public void setHotelsCount(int hotelsCount) {
+        this.hotelsCount = hotelsCount;
     }
 }
