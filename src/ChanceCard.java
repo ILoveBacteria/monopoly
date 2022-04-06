6c91f4bd027b9bd30570a4787429e46594182fbe
Moein Arabi <moein.mo81@gmail.com>
2022-04-06 19:20:17 +0430

    Added final variables to ChanceCard class

--- src/ChanceCard.java
+++ src/ChanceCard.java
@@ -1,30 +1,39 @@
 import java.util.Random;
 
 public class ChanceCard extends Area {
+    public static final int COLLECT_200 = 1;
+    public static final int GO_IN_PRISON = 2;
+    public static final int PAY_TAX = 3;
+    public static final int GO_FORWARD_3 = 4;
+    public static final int OPPORTUNITY_FREEDOM = 5;
+    public static final int DONT_PAY_TAX = 6;
+    public static final int PAY_10_TO_PLAYERS = 7;
+
+
     public ChanceCard(int areaNumber) {
         super(areaNumber, false);
     }
 
     public void randomChoiceCard(Player player) {
         switch (new Random().nextInt(7) + 1) {
-            case 1:
+            case COLLECT_200:
                 player.collectRent(200);
                 break;
-            case 2:
+            case GO_IN_PRISON:
                 player.setLocation(13);
                 player.inJail = true;
                 break;
-            case 3:
+            case PAY_TAX:
                 player.payTax();
                 break;
-            case 4:
+            case GO_FORWARD_3:
                 player.setLocation(player.getLocation() + 3);
                 break;
-            case 5:
+            case OPPORTUNITY_FREEDOM:
                 break;
-            case 6:
+            case DONT_PAY_TAX:
                 break;
-            case 7:
+            case PAY_10_TO_PLAYERS:
                 break;
         }
     }
