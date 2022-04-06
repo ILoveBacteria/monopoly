842329406b60847f9b852f6aee97b0883e45ba68
Moein Arabi <moein.mo81@gmail.com>
2022-04-06 14:01:32 +0430

    Added 4 of 7 chance cards

--- src/ChanceCard.java
+++ src/ChanceCard.java
@@ -8,12 +8,17 @@ public class ChanceCard extends Area {
     public void randomChoiceCard(Player player) {
         switch (new Random().nextInt(7) + 1) {
             case 1:
+                player.collectRent(200);
                 break;
             case 2:
+                player.setLocation(13);
+                player.inJail = true;
                 break;
             case 3:
+                player.payTax();
                 break;
             case 4:
+                player.setLocation(player.getLocation() + 3);
                 break;
             case 5:
                 break;
