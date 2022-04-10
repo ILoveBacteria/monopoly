85729fd2f45b252bd45a555fcf82c4c06605109b
Moein Arabi <moein.mo81@gmail.com>
2022-04-10 14:10:08 +0430

    Added swapWealth method

--- src/Bank.java
+++ src/Bank.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bank extends Area {
     private final ArrayList<Player> investors = new ArrayList<>();
@@ -17,5 +18,14 @@ public class Bank extends Area {
         int index = investors.indexOf(player);
         moneyInvested.remove(index);
         investors.remove(index);
+    }
+
+    public static void swapWealth(Player player1, Player player2) {
+        ArrayList<Area> areaTemp = player1.getRealEstates();
+        int assetTemp = player1.getAsset();
+        player1.setRealEstates(player2.getRealEstates());
+        player1.setAsset(player2.getAsset());
+        player2.setRealEstates(areaTemp);
+        player2.setAsset(assetTemp);
     }
 }
